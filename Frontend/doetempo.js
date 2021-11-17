b4240469b9c11403067a7090077e06af020a31f4
Rômulo Severo <romulo_severo@hotmail.com>
2021-11-17 07:08:40 -0300

    teste

--- Frontend/doetempo.js
+++ Frontend/doetempo.js
@@ -1,49 +1,57 @@
 
 function cadastroUser1() {
 
-    var data = {};
-    data.Email = document.querySelector('input[name=email]');
-    data.Senha = document.querySelector('input[name=senha]');
-    data.Nome = document.querySelector('input[name=nome]');
-    data.Sobrenome = document.querySelector('input[name=sobrenome]');
-    data.Idade = document.querySelector('input[name=idade]');
-    data.Celular = document.querySelector('input[name=celular]');
-    data.Cidade = document.querySelector('input[name=cidade]');
-    data.Estado = document.querySelector('input[name=estado]');
+    var data1 = {};
+    data1.Email = document.querySelector('input[name=email]');
+    data1.Senha = document.querySelector('input[name=senha]');
+    data1.Nome = document.querySelector('input[name=nome]');
+    data1.Sobrenome = document.querySelector('input[name=sobrenome]');
+    data1.Idade = document.querySelector('input[name=idade]');
+    data1.Celular = document.querySelector('input[name=celular]');
+    data1.Cidade = document.querySelector('input[name=cidade]');
+    data1.Estado = document.querySelector('input[name=estado]');
 
     fetch("https://localhost:44353/CadastroUsuario/", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify(data1),
     })
         .then((response) => response.json())
-        .then((data) => {
-            console.log("Success:", data);
+        .then((data1) => {
+            console.log("Success:", data1);
         })
         .catch((error) => {
             console.error("Error:", error);
         });
 
 
-    $(document).ready(function () {
-        // aqui vai seu código já criado
-
-        $("#click").on("click", function () { // Quando o elemento do id "click" é clicado...
-            var id_cart = $("#produtos_id").val(); // Pega o valor do campo produtos_id
-            var total_venda = $(".total_venda").text(); // Pega o total
-
-            $.ajax({
-                method: "POST",
-                url: "servidor.php",
-                data: { idCart: id_cart, totalVenda: total_venda } // Dados a serem enviados
-            });
-        });
-    });
 
 }
 
+$(document).ready(function () {
+    // aqui vai seu código já criado
+    $("#submit").on("click", function () { // Quando o elemento do id "click" é clicado...
+        var id_cart = $("#produtos_id").val(); // Pega o valor do campo produtos_id
+        var total_venda = $(".total_venda").text(); // Pega o total
+        var data1 = {};
+        data1.Email = document.querySelector('input[name=email]');
+        data1.Senha = document.querySelector('input[name=senha]');
+        data1.Nome = document.querySelector('input[name=nome]');
+        data1.Sobrenome = document.querySelector('input[name=sobrenome]');
+        data1.Idade = document.querySelector('input[name=idade]');
+        data1.Celular = document.querySelector('input[name=celular]');
+        data1.Cidade = document.querySelector('input[name=cidade]');
+        data1.Estado = document.querySelector('input[name=estado]');
+        $.ajax({
+            method: "POST",
+            url: "https://localhost:44353/CadastroUsuario/",
+            data: json.stringify(data1) // Dados a serem enviados
+        });
+    });
+
+});
 
 
 
